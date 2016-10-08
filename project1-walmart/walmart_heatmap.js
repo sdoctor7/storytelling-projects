a1f250ab091ec631fdf493d8fcc756193a5be37d
sdoctor7 <sdoctor7@users.noreply.github.com>
2016-10-08 16:12:27 -0400

    Add files via upload

--- project1-walmart/walmart_heatmap.js
+++ project1-walmart/walmart_heatmap.js
@@ -10,11 +10,18 @@
 				.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var xPositionScale = d3.scalePoint().range([0, width]).padding(1);
-	var yPositionScale = d3.scalePoint().range([height, 0]).padding(1);
+	var xPositionScale = d3.scalePoint().range([10, width]).padding(1);
+	var yPositionScale = d3.scalePoint().range([0, height-10]).padding(1);
+	var colorScale = d3.scaleLinear().range(['#fffcf7', '#f7eda3', 'red'])
 
-	var colorScale = d3.scaleLinear().range(['#fffcf7', 'red'])
+	var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return "<strong>Frequency:</strong> <span style='color:red'>" + d.Frequency + "</span>";
+    })
 
+	svg.call(tip);
 
 	d3.queue()
 		.defer(d3.csv, "conf_matrix_melt_10.6.csv")
@@ -23,7 +30,7 @@
 	function ready(error, datapoints) {
 
 		var maxfreq = d3.max(datapoints, function(d) {return d.Frequency});
-		colorScale.domain([0,maxfreq])
+		colorScale.domain([0,1,maxfreq])
 
 		var xUnique = d3.map(datapoints, function(d) {return d.Predicted_Product}).keys();
 		xPositionScale.domain(xUnique)
@@ -48,49 +55,31 @@
 				}
 				else {return "0.5px"}
 			})
+			.on('mouseover', tip.show)
+			.on('mouseout', tip.hide)
 
-		// svg.selectAll("text")
-		// 	.data(datapoints)
-		// 	.enter().append("text")
-		// 	.text(function(d) {return d.Country})
-		// 	.attr("x", function(d) {return xPositionScale(d.Year)})
-		// 	.attr("y", function(d) {return yPositionScale(d.Value)})
-		// 	.attr("dx", 8)
-		// 	.attr("dy", 5)
-		// 	.attr("fill", function(d) {
-		// 		if (d.Year == 2014 && d.Country == "Norway") {return("blue")}
-		// 		else {return "none"}
-		// 	})
-		// 	.attr("font-size", "12px")
-
-		// Add your axes
 		var xAxis = d3.axisBottom(xPositionScale)
-		var xAxisHeight = height-5;
-		var xLabelHeight = xAxisHeight-5;
+		var xAxisHeight = height-15;
+		var xLabelHeight = height+25;
     svg.append("g")
       .attr("class", "axis x-axis")
       .attr("transform", "translate(8," + xAxisHeight + ")")
       .call(xAxis);
 
-		svg.append('g')
-			.attr("class", "label")
+		svg.append('text')
 			.attr("text-anchor", "middle")
-			.attr('x', 10)
-			.attr('y', xLabelHeight)
+			.attr("transform", "translate(" + width/2 + "," + xLabelHeight + ")")
 			.text("Predicted Product");
 
     var yAxis = d3.axisLeft(yPositionScale);
     svg.append("g")
       .attr("class", "axis y-axis")
-			.attr("transform", "translate(20,8)")
+			.attr("transform", "translate(30,8)")
       .call(yAxis);
 
-		svg.append('g')
-			.attr("class", "label")
+		svg.append('text')
 			.attr("text-anchor", "middle")
-			.attr('x', 10)
-			.attr('y', 10)
-			.attr("transform", "rotate(-90)")
+			.attr("transform", "translate(0," + height/2 + ")rotate(-90)")
 			.text("Most Popular Product");
   }
 })();
